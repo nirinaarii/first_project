84723f58ed69f1589c0ae7a7bb3400f423cf3f1e
ubuntu <ubuntu@mail.seit.mg>
2024-07-29 15:50:07 +0200

    upload

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,17 +2,55 @@ import { Component } from '@angular/core';
 import { RouterOutlet,RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
 import { HammerModule } from '@angular/platform-browser';
-import { IgxCalendarModule } from 'igniteui-angular';
+import { IgxCalendarModule, IViewChangeEventArgs } from 'igniteui-angular';
 import { IGX_CALENDAR_DIRECTIVES } from 'igniteui-angular';
+import { CalendarView } from 'angular-calendar';
+import { DateAdapter } from '@angular/material/core';
 @Component({
   selector: 'app-header',
-  template :'<igx-calendar></igx-calendar>',
+  template :'<igx-calendar [value]="selectedDate"(onDateSelected)="highlight($event)"></igx-calendar>',
+  
   standalone: true,
-  imports: [ RouterLink,RouterOutlet,HammerModule,
+  imports: [RouterLink,RouterOutlet,HammerModule,
     IgxCalendarModule,IGX_CALENDAR_DIRECTIVES],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+   
+ public onSelection(dates: Date[]){
+   if(dates.length > 5){
+    // this.calendar.selectedDate = [];
+ }
+}
+  
+ public viewDateChanged(event:IViewChangeEventArgs){
+      
+ }
+  
+
+ public activeViewChanged(event:CalendarView){
+
+ }
+
+ techniciansAvailable = 3;
+
+getSpecialDates(): Date[]{
+      const today = new Date();
+
+      return [today];
+ }
+
+getSpecialDateStyle(date: Date): string{
+  if(date.getDate() === new Date().getDate()){
+    if(this.techniciansAvailable >= 3){
+      return 'background-color:green';
+    }else if(this.techniciansAvailable === 2){
+      return 'background-color:blue';
+    }
+     return 'background-color : red';
+  }
+  return '';
+ }
 
 }
